387b5669c299a0a033ba7be6911fb658c9ea220a
Adrian <chopon007@gmail.com>
2022-10-18 12:19:51 +0200

    Ejercicio9 added

---
+++ Ejercicio9.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * Realiza un programa que nos diga cuántos digitos tiene un número introducido por teclado.
+ * 
+ * @author Adrián Perogil Fernández
+ * 
+ */
+import java.util.Scanner;
+ public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un número: ");
+        int numero = sc.nextInt();
+        int aux = numero;
+        int i = 0;
+        do {
+            i++;
+            numero = numero / 10;
+        } while (numero != 0);
+        System.out.println(aux + " tiene " + i + " cifras.");
+        sc.close();
+    }
+ }
