701d5fd274e2ae56c0e55b50fa15f9cb4b5fbf22
Adrian <chopon007@gmail.com>
2022-10-19 22:33:01 +0200

    Ejercicio17 added

---
+++ Ejercicio17.java
@@ -0,0 +1,30 @@
+/*
+ * 
+ * Realiza un programa que sume los 100 números siguientes a un número entero positivo
+ * introducido por teclado. Se debe comprobar que el dato introducido es correcto (que es
+ * un número positivo).
+ * 
+ * @author Adrián Perogil Fernández
+ * 
+ */
+import java.util.Scanner;
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int numero = sc.nextInt();
+        int sumaTotal = 0;
+        // Comprobación de errores
+        if (numero < 0) {
+            System.out.println("Por favor, introduzca un número entero positivo");
+        }
+        for ( int i = numero ; i < 100 + numero ; i++ ) {
+            sumaTotal += i;
+            System.out.print(sumaTotal + " ");
+            System.out.println("");
+            System.out.print(i + " ");
+        }
+        System.out.println("Suma total: " + sumaTotal);
+        sc.close();
+    }
+}
