0e8763507668f8cf2fb99b5603fdcfba191b8ce3
Adrian <chopon007@gmail.com>
2022-10-21 17:09:59 +0200

    Ejercicio28 added

---
+++ Ejercicio28.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * Escribe un programa que calcule el factorial de un número entero
+ * leído por teclado.
+ * 
+ * @author Adrián Perogil Fernández
+ * 
+ */
+import java.util.Scanner;
+public class Ejercicio28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int numero = sc.nextInt();
+        int factorial = 1;
+        for ( int i = 1 ; i <= numero ; i++ ) {
+            factorial = factorial * i;
+
+        }
+        System.out.println(factorial);
+        sc.close();
+    }
+}
